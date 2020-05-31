a20574bbd4273b00c6bcd3ce569c362b45babcb5
Bartlomiej Styczen <bstyczen96@gmail.com>
2020-05-31 18:50:55 +0200

    Change method of storing training data in "prepare_data.py" script; moved "architecture_engineering.ipynb" to root directory

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # speed-challenge
 Solution to Comma.ai speed prediction challenge
+
+Have a look at notebook **architecure_engineering.ipynb** which is a playground with Tensorflow methods like encoding PNGs, shuffling or batching and prefetching which is used to load data in the background while model is training. 
+
+Look at **predict_from_three_frames.ipynb** to check current solution for predicting speed from images from dashcam. Model uses three consecutive frames and predicts speed for the middle frame. In this solution to estimate speed, model is using frame from the previous timestamp, current frame and next frame. 
+
+One problem of this solution is that there is one frame delay for speed which is not the big problem if framerate is high or car is moving slowly but it might results in problems when car is moving fast.
+
+To avoid this problem, model can be changed so that it uses only previous and current frame so there would not be any delay because of chosen architecture design.
--- prepare_data.py
+++ prepare_data.py
@@ -2,51 +2,69 @@
 import cv2
 import pickle
 import os
+import sys
 
 WIDTH = 640
 HEIGHT = 480
-TOP_LINE = 70
-BOTTOM_LINE = 340
+# The goal is to get 320x240 image
+TOP_LINE = 85
+BOTTOM_LINE = 325
+LEFT_LINE = 160
+RIGHT_LINE = WIDTH - 160
 
-def get_frame(capture):
-    frame_nr = capture.get(cv2.CAP_PROP_POS_FRAMES)
-    ret, frame = capture.read()
-    return ret, frame, int(frame_nr)
+DIR = os.path.join('data', 'images')
+
+#def get_frame(capture):
+#    frame_nr = capture.get(cv2.CAP_PROP_POS_FRAMES)
+#    ret, frame = capture.read()
+#    return ret, frame, int(frame_nr)
 
 try:
-    os.mkdir('images')
+    os.mkdir(DIR)
 except FileExistsError:
     pass
 
 # Load file with car speeds
-with open('data/train.txt', 'r') as f:
-    speeds = f.readlines()
+with open(os.path.join('data', 'train.txt'), 'r') as f:
+    speeds_lst = f.readlines()
 
 # Create video capture to read frames of video
-cap = cv2.VideoCapture('data/train.mp4')
+cap = cv2.VideoCapture(os.path.join('data', 'train.mp4'))
 nr_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-image_paths = [] 
-speeds = [float(speed) for speed in speeds]
+# Check length validity of data.
+if nr_frames != len(speeds_lst):
+    print('Number of frames in a video does not match number of target speeds. Exiting...')
+    sys.exit()
 
-while True:
-    ret, image, frame_nr = get_frame(cap)
+# Structures with data
+images = []
+speeds = {}
+
+for frame_nr in range(len(speeds_lst)):
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_nr)
+    ret, image = cap.read()
+    
     if not ret:
-        print(f'\nDone')
+        print('\nError reading frames.')
         break
-
+   
     print(f'{frame_nr + 1} / {nr_frames}', end='\r')
+    image_name = f'{frame_nr}.png'
 
-    # Crop image, save its relative path to list
-    image_cropped = image[TOP_LINE:BOTTOM_LINE, ...]  
-    image_rel_path = os.path.join('images', f'{frame_nr}.png')
-    image_paths.append(image_rel_path) 
+    # Crop image
+    image_cropped = image[TOP_LINE:BOTTOM_LINE, LEFT_LINE:RIGHT_LINE, :]  
+   
+    # Log
+    images.append(image_name)
+    speeds[image_name] = float(speeds_lst[frame_nr])
 
     # Save image to directory to PNG
-    cv2.imwrite(image_rel_path, image_cropped)
+    cv2.imwrite(os.path.join(DIR, image_name), image_cropped)
 
-# Save images to pickle
-pickle.dump({'image_paths': image_paths, 'targets': speeds}, open('train.pickle', 'wb'))
+# Save dataset to pickle
+pickle.dump({'x': images, 'y': speeds}, open(os.path.join('data', 'train.pickle'), 'wb'))
 
+# Release VideoCapture
 cap.release()
 
